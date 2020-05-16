bbdd1e129e356290e83a90c0611f70ce3b8276f8
serhiiko <serhiiko@serhiiko-mbp15.local>
2020-05-16 13:31:16 +0300

    add list rendering

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const store = Redux.createStore(rootReducer);
-console.log(store.getState());
-store.dispatch(actions.addTodo('afadd'));
-console.log(store.getState());
-store.dispatch(actions.deleteTodo(1));
-console.log(store.getState());
+const list = document.querySelector('.todo-list');
+
+const render = () => {
+    const todos = store.getState().todos;
+    const html = todos.map((todo) => {
+        const completedClass = todo.completed ? 'completed' : '';
+        return `<li data-id="${todo.id}" class="${completedClass}">
+                    <div class="view">
+                        <input class="toggle" type="checkbox" /><label>${todo.title}</label>
+                        <button class="destroy"></button>
+                    </div>
+                </li>`;
+    });
+    
+    list.innerHTML = html;
+}
+
+render();
+store.subscribe(render);
+// console.log(store.getState());
+// store.dispatch(actions.addTodo('afadd'));
+// console.log(store.getState());
+// store.dispatch(actions.deleteTodo(1));
+// console.log(store.getState());
